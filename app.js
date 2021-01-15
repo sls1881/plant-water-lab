3a3bd24ea6671502119600a2f03915c660fbc0d9
moises depena <swe.moisesdepe@gmail.com>
2021-01-14 17:43:46 -0800

    refactoring

--- app.js
+++ app.js
@@ -7,20 +7,41 @@ const numButton = document.getElementById('num-button');
 
 const highLowSpan = document.getElementById('high-low');
 
+const correctAnswer = document.getElementById('correct-answer');
+
 const guessesRemainingSpan = document.getElementById('guesses-remaining');
 
 // initialize state
 let guessesRemaining = 4;
 
-let answerNumber = (Math.ceil(Math.random()) * 20);
+let answerNumber = Math.ceil(Math.random() * 20);
 
 // set event listeners to update state and DOM
 numButton.addEventListener('click', () => {
+
     //Decrement # of guesses
     guessesRemaining--;
 
     const userGuess = numInput.valueAsNumber;
 
+
+    messageContent(userGuess, answerNumber);
+    
+    remainingGuesses(guessesRemaining); 
+
+});
+
+function remainingGuesses(guessesRemaining) {
+
+    if (guessesRemaining > 0) {
+        guessesRemainingSpan.textContent = `You have ${guessesRemaining} guesses remaining`;
+    } else if (guessesRemaining <= 0) {
+        guessesRemainingSpan.textContent = "You're all out of turns. You lose.";
+        correctAnswer.textContent = `The correct answer was ${answerNumber}.`;
+    }
+}
+function messageContent(userGuess, answerNumber) {
+    
     if (compareNumbers(userGuess, answerNumber) === 0) {
         highLowSpan.textContent = "You're correct!";
         guessesRemainingSpan.textContent = 'Congratulations!';
@@ -31,12 +52,4 @@ numButton.addEventListener('click', () => {
     } else if (compareNumbers(userGuess, answerNumber) === 1) {
         highLowSpan.textContent = 'Your guess is too high.';
     }
-
-    if (guessesRemaining > 0) {
-        guessesRemainingSpan.textContent = `You have ${guessesRemaining} guesses remaining`;
-    } else if (guessesRemaining <= 0) {
-        guessesRemainingSpan.textContent = "You're all out of turns. You lose.";
-    }
-
-});
-
+}
