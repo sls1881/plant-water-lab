f2452a2e5a4ab8ff3a20953fe4c0c802849b851a
‘Shelby <sls1881@gmail.com>
2021-01-16 12:56:10 -0800

    added a reset button and refactor

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ const highLowSpan = document.getElementById('high-low');
 
 const guessesRemainingSpan = document.getElementById('guesses-remaining');
 
+const resetButton = document.getElementById('reset-button');
+
 // initialize state
 let guessesRemaining = 4;
 
-let answerNumber = (Math.ceil(Math.random()) * 20);
+let answerNumber = Math.ceil(Math.random() * 20);
 
 // set event listeners to update state and DOM
 numButton.addEventListener('click', () => {
@@ -21,9 +23,38 @@ numButton.addEventListener('click', () => {
 
     const userGuess = numInput.valueAsNumber;
 
+    messageOutput(userGuess);
+
+    numGuessesRemaining(guessesRemaining);
+
+});
+
+resetButton.addEventListener('click', () => {
+
+    numButton.style.display = '';
+    guessesRemaining = 4;
+    guessesRemainingSpan.textContent = '';
+    numInput.value = '';
+    numButton.disabled = false;
+    answerNumber = Math.ceil(Math.random() * 20);
+    highLowSpan.textContent = '';
+    resetButton.style.display = 'none';
+
+});
+
+function numGuessesRemaining() {
+    if (guessesRemaining > 0) {
+        guessesRemainingSpan.textContent = `You have ${guessesRemaining} guesses remaining`;
+    } else if (guessesRemaining <= 0) {
+        guessesRemainingSpan.textContent = "You're all out of turns. You lose.";
+    }
+}
+
+function messageOutput(userGuess) {
     if (compareNumbers(userGuess, answerNumber) === 0) {
         highLowSpan.textContent = "You're correct!";
-        guessesRemainingSpan.textContent = 'Congratulations!';
+        guessesRemainingSpan.style.display = 'none';
+        numButton.style.display = 'none';
     }
 
     if (compareNumbers(userGuess, answerNumber) === -1) {
@@ -31,12 +62,5 @@ numButton.addEventListener('click', () => {
     } else if (compareNumbers(userGuess, answerNumber) === 1) {
         highLowSpan.textContent = 'Your guess is too high.';
     }
-
-    if (guessesRemaining > 0) {
-        guessesRemainingSpan.textContent = `You have ${guessesRemaining} guesses remaining`;
-    } else if (guessesRemaining <= 0) {
-        guessesRemainingSpan.textContent = "You're all out of turns. You lose.";
-    }
-
-});
+}
 
